02db4ce1637ad33e155fa1a33b7cdda6004fd82c
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-05-19 21:59:30 +0430

    In the Name of Allah

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const minify = require('gulp-minify');
+const concat = require('gulp-concat');
+
+function css() {
+    return src('src/scss/jamal.scss')
+        .pipe(sass())
+        .pipe(minifyCSS())
+        .pipe(dest('dist/css'))
+}
+
+function js() {
+    return src('src/js/**/*.js', {sourcemaps: true})
+        .pipe(concat('jamal.js'))
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+        }))
+        .pipe(dest('dist/js', { sourcemaps: false }))
+}
+
+function watchFile() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', js);
+}
+
+exports.js = js;
+exports.css = css;
+exports.watchFile = watchFile;
+exports.default = parallel(js, css, watchFile);
